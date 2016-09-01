c451cab3013ef69f35c508c1fba0ade52f2f7082
Stefan <moldovanst@yahoo.com>
2016-09-01 18:03:29 +0000

    fist version of login

---
+++ application/views/loginview.php
@@ -0,0 +1,6 @@
+<div id = 'login'>
+    <input id="email-input" type="text" name="email" placeholder="Type your email"/>
+    <input id="username-input" type="text" name="username" placeholder="Type your username"/>
+    <input id="password-input" type="password" name="psw" placeholder="Your password"/>
+    <button id="submit-btn" class = "buttons">Login</button>
+</div>
