b88932cb4d5e7c89dfdfe917763bb246afeb6eca
Adrian <matei.adrian25@gmail.com>
2016-09-01 17:15:52 +0000

    assets folder, jquery in layout and 1st password safety check

--- application/views/createview.php
+++ application/views/createview.php
@@ -1,1 +1,7 @@
 <h1>Create account page</h1>
+<form>
+  <input type="text" name="firstName" placeholder="Enter your first name"/>
+  <input type="text" name="lastName" placeholder="Enter your last name"/>
+  <input type="password" data-id="create-pass" name="password" placeholder="Parola*"/>
+  <button id="submit-btn">Submit</button>
+</form>
--- application/views/layout.php
+++ application/views/layout.php
@@ -4,19 +4,24 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+
     <title><?php echo $title;?></title>
 </head>
-    <body>
-        <header>
-            header
-        </header>
-        
-        <div class="content">
-            <?php include $pageContent; ?>
-        </div>
-        
-        <footer>
-            footer
-        </footer>
-    </body>
+<body>
+    <header>
+        header
+    </header>
+    
+    <div class="content">
+        <?php include $pageContent; ?>
+    </div>
+    
+    <footer>
+        footer
+    </footer>
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.2.1.js"></script>
+    <script type="text/javascript" src="assets/js/passwordsafety.js"></script>
+</body>
 </html>
