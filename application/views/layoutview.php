1975b5189c430664635cf4283e1efe96c45879ba
Cornea Andrei <cornea_andrei_l@yahoo.com>
2016-09-01 16:03:48 +0000

    proba

---
+++ application/views/layoutview.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+    
+    <header></header>
+    
+    <div class="content">content</div>
+    
+    <footer>footer</footer>
+
+</body>
+</html>
