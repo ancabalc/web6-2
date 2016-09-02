00f1947ce566f91e134948a0b6c68c4855c1ffa7
adrianmq <mateiadrian25@gmail.com>
2016-09-02 23:53:43 +0000

    password strength criteria improved

--- assets/js/passwordsafety.js
+++ assets/js/passwordsafety.js
@@ -12,36 +12,37 @@ $(document).ready(function(){
   }
 
   // definition of password strength
+  // the input has to meet the criteria set by the regex's for improvement
   var inputValidation = [
-    { // under 4 char,
+    { // under 4 char, any char. - still weak
       type: 'unaccepted',
       color: '#c9302c',
       line: '75px',
-      regex: /^([\d\D]){1,4}$/
+      regex: /^.{1,4}$/
     },
-    { // under 6 char, has to improved
+    { // btw. 5 and 8 will match only [a-zA-Z0-9_]
       type: 'weak',
       color: '#ffb31a',
       line: '150px',
-      regex: /^([\d\D]){5,8}$/
+      regex: /^([a-zA-Z0-9_]){5,8}$/
     },
-    { // under 8 char, has to improved
+    { // btw. 5 and 8, pass must contain \w\d\W // btw. 9 and 12 will match only [a-zA-Z0-9_]
       type: 'good',
       color: '#47d147',
       line: '225px',
-      regex: /^([\d\D]){9,12}$/
+      regex: /^((?=(?:.{9,12})$)([a-zA-Z0-9_]+)|(?=(?:.{5,8})$)((\W*[\w\d]+\W+.*)|([\w\d]*\W+[\w\d]+.*)))$/
     },
-    { // 10+ char, has to improved
+    { // btw. 9 and 12, pass must contain \w\d\W // btw. 13 and 19 will match only [a-zA-Z0-9_]
       type: 'strong',
       color: '#1a75ff',
       line: '300px',
-      regex: /^([\d\D]){13,19}$/
+      regex: /^((?=(?:.{13,19})$)([a-zA-Z0-9_]+)|(?=(?:.{9,12})$)((\W*[\w\d]+\W+.*)|([\w\d]*\W+[\w\d]+.*)))$/
     },
-    { // >20 char, has to improved
+    { // >20 char // 13 -> 19 with \w\d\W -> take it as text
       type: 'surpassed',
       color: '#000a1a',
       line: '300px',
-      regex: /^([\d\D]){20,}$/
+      regex: /^((.{20,}$)|(?=(?:.{13,19})$)((\W*[\w\d]+\W+.*)|([\w\d]*\W+[\w\d]+.*)))$/
     },
   ]
 
@@ -52,55 +53,54 @@ $(document).ready(function(){
     // take value from field
     var inputVal = $passField.val();
 
-    // iterate over each predefined password pattern
-    $.each(inputValidation, function(currentMatchedIndex, obj){
-
-      // check regex match
-      if(inputVal.match(obj.regex)){
-
-        // iterate over meter elements
-        $.each($meterElements, function(elemIndex, meterElem){
-          // assing corresponding color
-          $(meterElem).css('background-color', obj.color);
-          // break each loop match index
-          if( elemIndex === currentMatchedIndex ){ return false; }
-        });
-
-        // when the user starts to erase the password, the coloring has to be reset to the default value
-        // the second condition is for avoinding assignment to undefined pass-meter elem, since there is the 5th case for surpassed
-        if(( globMatchedIndex > currentMatchedIndex ) && ( globMatchedIndex < $meterElements.length )){
-
-          // get pass-meter last colored element after match
-          var meterElem = $meterElements[globMatchedIndex];
-
-          // assign default color
-          $(meterElem).css('background-color', 'lightgray');
+    if( inputVal ){
+      // iterate over each predefined password pattern
+      $.each(inputValidation, function(currentMatchedIndex, obj){
+        // check regex match
+        if(inputVal.match(obj.regex)){
+          // iterate over meter elements
+          $.each($meterElements, function(elemIndex, meterElem){
+            // assing corresponding color
+            $(meterElem).css('background-color', obj.color);
+            // break each loop match index
+            if( elemIndex === currentMatchedIndex ){ return false; }
+          });
+  
+          // when the user starts to erase the password, the coloring has to be reset to the default value
+          // the second condition is for avoinding assignment to undefined pass-meter elem, since there is the 5th case for surpassed
+          if(( globMatchedIndex > currentMatchedIndex ) && ( globMatchedIndex < $meterElements.length )){
+  
+            // get pass-meter last colored element after match
+            var meterElem = $meterElements[globMatchedIndex];
+  
+            // assign default color
+            $(meterElem).css('background-color', 'lightgray');
+          }
+  
+          // assign deduced strength type
+          $passStrInfo.text(obj.type).css('color', obj.color);
+  
+          // disable create account button, because password is too long
+          if(( currentMatchedIndex == $meterElements.length ) && ( typeof $createButton.attr('disabled') === typeof undefined )){
+            // disable button
+            $createButton.attr('disabled', 'disabled');
+          }
+          else if(( typeof $createButton.attr('disabled') === typeof undefined ) && ( currentMatchedIndex == 0 )){
+            // disable button
+            $createButton.attr('disabled', 'disabled');
+          }
+          else if(( typeof $createButton.attr('disabled') !== typeof undefined ) && ( currentMatchedIndex > 0 ) && ( currentMatchedIndex < $meterElements.length )){
+            // remove attribute disabled
+            $createButton.removeAttr('disabled');
+          }
+  
+          // store index as global variable to know which was the last match
+          globMatchedIndex = currentMatchedIndex;
         }
-
-        // assign deduced strength type
-        $passStrInfo.text(obj.type).css('color', obj.color);
-
-        // disable create account button, because password is too long
-        if(( currentMatchedIndex == $meterElements.length ) && ( typeof $createButton.attr('disabled') === typeof undefined )){
-          // disable button
-          $createButton.attr('disabled', 'disabled');
-        }
-        else if(( typeof $createButton.attr('disabled') === typeof undefined ) && ( currentMatchedIndex == 0 )){
-          // disable button
-          $createButton.attr('disabled', 'disabled');
-        }
-        else if(( typeof $createButton.attr('disabled') !== typeof undefined ) && ( currentMatchedIndex > 0 ) && ( currentMatchedIndex < $meterElements.length )){
-          // remove attribute disabled
-          $createButton.removeAttr('disabled');
-        }
-
-        // store index as global variable to know which was the last match
-        globMatchedIndex = currentMatchedIndex;
-      }
-    });
-    
+      });
+    }
     // when user clears field
-    if( ! $passField.val() ){
+    else {
       // assign default color to all meter elements
       $meterElements.css('background-color', 'lightgray');
 
