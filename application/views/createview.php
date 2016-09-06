aef7ebfa1968d18979328daec27638655bc8bfc9
Cristi <cristian.pasca@mideali.com>
2016-09-06 17:19:23 +0000

    Login using Facebook account

--- application/views/createview.php
+++ application/views/createview.php
@@ -1,1 +1,18 @@
 <h1>Create account page</h1>
+
+
+
+<!--Login using Facebook account-->
+<div id="fb-root"></div>
+
+<script>
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1285490414803865";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+    
+<div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="true"></div>
