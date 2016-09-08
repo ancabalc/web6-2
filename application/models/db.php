6d21b6efe5b0a08d9061679734fc3464acd778c0
Cornea Andrei <cornea_andrei_l@yahoo.com>
2016-09-08 17:06:18 +0000

    model db

--- application/models/db.php
+++ application/models/db.php
@@ -1,20 +1,22 @@
 <?php    
 
-    define("MySQL_USER", "root");
-    define("MySQL_PASSWORD", "");
-    define("MySQL_DB_NAME", "db");
-    define("MySQL_HOST", "localhost");
-   
+
     class DB {
         protected $dbh;
         
         function __construct() {
+             $CI =& get_instance();
+             $CI->load->database();
             try {
-                // Set connection on class property
-                $this->dbh = new PDO('mysql:host='.MySQL_HOST.';dbname=' . MySQL_DB_NAME, MySQL_USER, MySQL_PASSWORD);     
+               $this->dbh = new PDO('mysql:host=' . $CI->db->hostname .';dbname=' . $CI->db->database, $CI->db->username, $CI->db->password);
             } catch (PDOException $e) {
-                print "Error!: Cannot connect to db! Check connection " . $e->getMessage() . "<br/>";
-                die();
-            }    
+                print "Error!:" . $e->getMessage() . "<br/>";
+            die();
+            }
         }
+    }
+    
+
+           
+        
 ?>
