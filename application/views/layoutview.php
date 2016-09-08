3ae6a1ade8356f15c52e3c2e66ab05e7bab9b9be
Cornea Andrei <cornea_andrei_l@yahoo.com>
2016-09-08 16:14:23 +0000

    create view

--- application/views/layoutview.php
+++ application/views/layoutview.php
@@ -1,17 +1,31 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <title><?php echo $title;?></title>
+    
+    <link rel="stylesheet" href="//project-web2-andreic15.c9users.io/assets/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+    <script type="text/javascript" src="//code.jquery.com/jquery-2.2.1.js"></script>
+    <script type="text/javascript" src="//project-web2-andreic15.c9users.io/assets/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+
 </head>
 <body>
+    <header>
+        header
+    </header>
     
-    <header></header>
+    <div class="content">
+        <?php include $pageContent; ?>
+    </div>
     
-    <div class="content">content</div>
-    
-    <footer>footer</footer>
-
+    <footer>
+        footer
+    </footer>
+    <script type="text/javascript" src="//project-web2-andreic15.c9users.io/assets/js/passwordsafety.js"></script>
 </body>
+
 </html>
