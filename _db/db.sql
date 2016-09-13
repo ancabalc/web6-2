dd748a5c5213bd5d4c4ea955c5f999ddbf53f586
siitwebcluj <paul.brie@scoalainformala.ro>
2016-09-13 14:43:20 +0000

    add new db tables

--- _db/db.sql
+++ _db/db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.12
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 06, 2016 at 06:12 PM
--- Server version: 5.5.36
--- PHP Version: 5.4.27
+-- Gazda: 127.0.0.1
+-- Timp de generare: 13 Sep 2016 la 14:41
+-- Versiune server: 5.5.50-0ubuntu0.14.04.1
+-- Versiune PHP: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -16,29 +16,78 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-CREATE DATABASE `webapp_services`;
-
-use `webapp_services`;
-
 --
--- Database: `db`
+-- Bază de date: `webapp_services`
 --
+CREATE DATABASE IF NOT EXISTS `webapp_services` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `webapp_services`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structura de tabel pentru tabelul `applications`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (\
+CREATE TABLE IF NOT EXISTS `applications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `categories`
+--
+
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Salvarea datelor din tabel `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`) VALUES
+(1, 'Meditatii'),
+(2, 'Curatenie & Menaj');
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `offers`
+--
+
+CREATE TABLE IF NOT EXISTS `offers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `details` text NOT NULL,
+  `active` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structura de tabel pentru tabelul `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL,
+  `town` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `role` enum('provider','client') NOT NULL,
-  PRIMARY KEY (`id`)
+  `role` enum('provider','client') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
