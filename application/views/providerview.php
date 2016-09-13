a382431f3c8e243cd64cb9e81186a0002ec625bd
Almasan <erika.almasan@yahoo.com>
2016-09-13 17:43:49 +0000

    user form, create profile

--- application/views/providerview.php
+++ application/views/providerview.php
@@ -2,5 +2,5 @@
     <input type = "text" name = "name" value = "Name">
     <input type="submit" value="Submit" class="submit-upload" disabled/>
     <textarea type = "description" value = "Description"></textarea>
-  
+    <input id="user-submit" type="submit" value="Save" />
 </form>
