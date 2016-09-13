6ad9cffcd2029fadbacf7ae79db6760ceedc5fc7
adrianmq <mateiadrian25@gmail.com>
2016-09-13 17:50:31 +0000

    get categories from table

--- application/controllers/Application.php
+++ application/controllers/Application.php
@@ -1,25 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-    var_dump($_POST);
+var_dump($_POST);
+// var_dump($_SERVER);
 
 class Application extends CI_Controller {
 
     public function create() {
+        require APPPATH . "models/applicationsmodel.php";
 
         $data['title'] = 'CREARE CERERE';
         $data['pageContent'] = "cerereview.php";
+        $data['categories'] = $this->categories();
+
+        var_dump($data['categories']);
+
         $this -> load -> view('layout', $data);
-
-        var_dump($_POST);
-        var_dump($_SERVER);
-
-
     }
     
-    
-    
-    
-    
-    
+    function categories() {
+        require APPPATH . "models/categoriesmodel.php";
+     
+        $categoriesModel = new CategoriesModel();
+        $result = $categoriesModel->getCategories();
+        
+        return $result;
+    }
 }
