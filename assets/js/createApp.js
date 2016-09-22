8a83a339326e17ac7f623a172752a39a442a88e3
adrianmq <mateiadrian25@gmail.com>
2016-09-22 17:58:21 +0000

    validation on application create inputs

--- assets/js/createApp.js
+++ assets/js/createApp.js
@@ -2,31 +2,71 @@
 $(document).ready(function(){
   var $createAppBtn = $("#createApp");
   
-  $createAppBtn.click(function(){
-    var $appSubject = $("#appSubject"),
-      $selectCategory = $("#selectCategory"),
-      $appDescription = $("#appDescription");
+  $createAppBtn.click(function(e){
+    var linkedFields = {
+      subject: 'appSubject',
+      category: 'selectCategory',
+      description: 'appDescription'
+    },
+    incompleteData = [],
+    completeData = {};
     
-    var categoryId = $selectCategory.val(),
-      categoryName = $selectCategory.find("option:selected").text(),
-      appSubject = $appSubject.val(),
-      appDescription = $appDescription.val();
+    $.map(linkedFields, function(selectorId, selectorName){
+      var $selector = $("#"+selectorId);
 
-    $.ajax({
-      type:"POST",
-      data:{
-        categoryId: categoryId,
-        categoryName: categoryName,
-        appSubject: appSubject,
-        appDescription: appDescription
-      },
-      url:"save", 
-      success: function(response) {   
-        alert(response.responseJSON.message);
-      },
-      error: function(response) {
-        alert(response.responseJSON.message);
+      if(( !$selector.val() || parseInt($selector.val(), 10) === 0 )){
+        
+        if( $selector.siblings("span").length === 0 ){
+          $selector.css('border-bottom', '3px solid #c9302c');
+          $selector.after(
+            '<span id="pass-meter-info" name="'+selectorName+'Warn" class="pass-meter-aux" style="width:300px; color:#c9302c;">'+
+            'Incomplete data for '+selectorName+'</span>'
+          );
+          $selector.after('<span id="pass-meter-icon" name="'
+            +selectorName+'Icon" class="pass-meter-aux"></span>'
+          );
+        }
+
+        incompleteData.push($selector);
+      }
+      else {
+        $selector.css('border-bottom', '1px solid #435160');
+        $selector.siblings().map(function(index, sibling){
+          var $jQelem = $(sibling);
+          if(($jQelem.attr('name') == selectorName+"Icon") || ($jQelem.attr('name') == selectorName+"Warn")){
+            
+            $jQelem.remove();
+          }
+        });
+        
+        completeData[selectorName] = $selector; 
       }
     });
+      
+    if( !incompleteData.length ){
+      var categoryId = completeData['category'].val(),
+        categoryName = completeData['category'].find("option:selected").text(),
+        appSubject = completeData['subject'].val(),
+        appDescription = completeData['description'].val();
+
+      $.ajax({
+        type:"POST",
+        data:{
+          categoryId: categoryId,
+          categoryName: categoryName,
+          appSubject: appSubject,
+          appDescription: appDescription
+        },
+        url:"save", 
+        success: function(response) {
+          // alert(response.responseJSON.message);
+          alert(response.message);
+        },
+        error: function(response) {
+          // alert(response.responseJSON.message);
+          alert(response.message);
+        }
+      });
+    }
   });
 })
